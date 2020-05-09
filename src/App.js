074201bdf5fa5ea4ae2cfc8708cd260e95fb6fe0
Maksim Koshman <maksimkoshman090@gmail.com>
2020-05-09 15:42:08 +0300

    app is ready

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoList from './Todo/TodoList'
+import Context from './context'
+import Loader from './Loader'
+
+const AddTodo = React.lazy(() => new Promise(resolve => {
+  setTimeout(() => {
+    resolve(import('./Todo/AddTodo'))
+  }, 3000)
+}))
 
 function App() {
-  const todos = [
-    {id: 1, completed: false, title: 'Купить хлеб'},
-    {id: 2, completed: false, title: 'Купить ветчину'},
-    {id: 3, completed: false, title: 'Купить сыр'}
-  ]
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
+
+  function toggleTOdo(id) {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
+
   return (
-    <div className="wrapper">
-      <h1>React Tutorial</h1>
-      <TodoList todos={todos} / >
-    </div>
+    <Context.Provider value={{removeTodo}}>
+      <div className="wrapper">
+        <h1>React Tutorial</h1>
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo}></AddTodo>
+        </React.Suspense>
+        {loading && <Loader />}
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTOdo} / > : 
+        loading ? null : (
+          <p>No todos</p>
+        )
+        }
+      </div>
+    </Context.Provider>
   );
 }
 
